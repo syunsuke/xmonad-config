5f6895b17b22b978429cfd08e707df7de2b76935
Syunsuke Fukuda <syunsuke.fukuda@gmail.com>
2021-03-02 12:49:39 +0900

    base xmonad.hs for logHook and logHookPP setting

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -7,10 +7,11 @@ import System.Exit
 
 import XMonad
 import XMonad.Util.Run
+import XMonad.Util.WorkspaceCompare
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 import qualified XMonad.StackSet as W
-
+import XMonad.Util.Loggers
 import XMonad.Util.EZConfig
 
 ---------------------------------------------------------------
@@ -26,15 +27,9 @@ main = do
     $ docks 
     $ def { terminal          = "kitty"
           , modMask           = mod4Mask
-          , focusFollowsMouse = False
           , workspaces        = map show [1..5 ::Int]
-          , borderWidth　     = 3
-          , normalBorderColor = "#cccccc"
-          , focusedBorderColor= "#00bbff"
-          , manageHook        = manageHook def
-          , handleEventHook   = handleEventHook def
-          , layoutHook        = mylayouthook
-          , logHook           = myXmobarLogHook h
+          , layoutHook        = avoidStruts (layoutHook def)
+          , logHook           = myLogHook h
           , keys              = \c -> mkKeymap c (keyMapDataList c)
           }
 
@@ -42,23 +37,23 @@ main = do
 -- ステータスバー表示のカスタマイズ
 ---------------------------------------------
 
--- xmobar用
-myXmobarLogHook h =
-  dynamicLogWithPP
-    xmobarPP 
-      { ppOutput  = hPutStrLn h 
-      , ppCurrent = xmobarColor "#FF9F1C" "#1A1B41" . pad . wrap "[" "]" 
-      , ppTitle   = xmobarColor "#1A1B41" "#C2E7DA" . shorten 50 . pad
+myLogHook h =
+  dynamicLogWithPP def
+      { ppOutput  = hPutStrLn h
+      , ppCurrent = wrap "[" "]"
+      , ppHidden   = id
+      , ppHiddenNoWindows = const ""
+      , ppUrgent  = id
+      , ppSep     = " : "
+      , ppWsSep   = " "
+      , ppTitle   = shorten 80
+      , ppTitleSanitize = xmobarStrip . dzenEscape
+      , ppLayout  = id
+      , ppOrder   = id
+      , ppSort    = getSortByIndex
+      , ppExtras  = []
       }
 
----------------------------------------------
--- レイアウト
----------------------------------------------
-mylayouthook = 
-  avoidStruts mytall ||| avoidStruts mymirror ||| myfull
-  where mytall   =  Tall 1 0.03 0.5
-        mymirror =  Mirror mytall
-        myfull   =  Full
 
 ---------------------------------------------
 -- キーバインド関連
@@ -71,19 +66,12 @@ keyMapDataList conf =
   ,("M-<Space>", sendMessage NextLayout)
   ,("M-S-<Space>", setLayout $  XMonad.layoutHook conf)
   ,("M-n", refresh)
-  ,("M-<KP_Tab>", windows W.focusDown)
-  ,("M-S-<KP_Tab>", windows W.focusUp)
   ,("M-j", windows W.focusDown)
   ,("M-k", windows W.focusUp)
   ,("M-m", windows W.focusMaster)
   ,("M-S-j", windows W.swapDown)
   ,("M-S-k", windows W.swapUp)
   ,("M-<Return>", windows W.swapMaster)
-  ,("M-h", sendMessage Shrink)
-  ,("M-l", sendMessage Expand)
-  ,("M-t", withFocused $ windows . W.sink)
-  ,("M-,", sendMessage $ IncMasterN 1)
-  ,("M-.", sendMessage $ IncMasterN (-1))
   ,("M-S-q", io (exitWith ExitSuccess))
   ,("M-q", spawn myRecompileCmd)
   ]
